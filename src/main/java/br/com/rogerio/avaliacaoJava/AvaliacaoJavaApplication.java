b5b65253c3aeb39c7a18400c1b0fe5413c0031ef
Enzoroge <rogeriolima20042000@yahoo.com.br>
2023-10-27 15:05:14 -0300

    Projeto criado

---
+++ src/main/java/br/com/rogerio/avaliacaoJava/AvaliacaoJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rogerio.avaliacaoJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaliacaoJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaliacaoJavaApplication.class, args);
+	}
+
+}
